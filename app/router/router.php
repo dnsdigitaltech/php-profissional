c54fdc1fbab9f7e896e259649bf1fcd1b57155b3
Davi Bernardo <davi.bernardo@sbahq.org>
2024-01-18 15:06:29 -0300

    Pegando os parâmetros da Uri nas rotas dinâmicas

--- app/router/router.php
+++ app/router/router.php
@@ -23,14 +23,38 @@ function regularExpressionMatchArrayRoutes($uri, $routes){
         ARRAY_FILTER_USE_KEY
     );
 }
+function params($uri, $matchedUri){
+    if(!empty($matchedUri)){
+        $matchedToGetParams = array_keys($matchedUri)[0];
+        return array_diff(
+            explode('/',ltrim($uri,'/')),
+            explode('/',ltrim($matchedToGetParams,'/'))
+        );
+    };
+    return [];
+}
+//get routers
 function router()
 {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
+
+    // $arr1 = [
+    //     'user',1,'name','davi'
+    // ];
+    
+    // $arr2 = [
+    //     'user','[0-9]+','name','[a-z]+'
+    // ];
+
+    // var_dump(array_diff($arr1,$arr2));
+
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
 
     if(empty($matchedUri)){
         $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+        
+        $params = params($uri, $matchedUri);
+            var_dump($params);
     }
-    var_dump($matchedUri);
 }
--- app/router/routes.php
+++ app/router/routes.php
@@ -3,4 +3,5 @@ return [
     '/' => 'Home@index',
     '/user/create' => 'User@create',
     '/user/[a-z0-9]+' => 'User@index',
+    '/user/[0-9]/name/[a-z]+' => 'User@index',
 ];
