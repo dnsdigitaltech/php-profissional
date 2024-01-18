be11e0aa75697582a17b0d1d2eb4c62444d01b57
Davi Bernardo <davi.bernardo@sbahq.org>
2024-01-18 14:46:01 -0300

    Pegando as rotas dinâmicas

--- app/router/router.php
+++ app/router/router.php
@@ -3,16 +3,34 @@ function routes()
 {
     return require 'routes.php';
 }
+//Uris
 function exactMatchUriInArrayRoutes($uri, $routes)
 {
     if(array_key_exists($uri, $routes)){
-        return [];
+        return [$uri => $routes[$uri]];
     }
     return [];
+}
+//Uris dinamicas
+function regularExpressionMatchArrayRoutes($uri, $routes){
+    return array_filter(
+        //array_keys($routes),
+        $routes,
+        function($value)use($uri){
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        },
+        ARRAY_FILTER_USE_KEY
+    );
 }
 function router()
 {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $routes = routes();
     $matchedUri = exactMatchUriInArrayRoutes($uri, $routes);
+
+    if(empty($matchedUri)){
+        $matchedUri = regularExpressionMatchArrayRoutes($uri, $routes);
+    }
+    var_dump($matchedUri);
 }
